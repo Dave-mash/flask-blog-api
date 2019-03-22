1befd8406c24d39b1fd917e1f07bca7b128464ab
Dave-mash <macharia3041@gmail.com>
2019-03-22 09:15:20 +0300

    modifications

--- app/database.py
+++ app/database.py
@@ -18,6 +18,7 @@ class InitializeDb:
             print(f'A connection to {db_url.DB_URL} database was established!')
             return cls.cursor
         except:
+            print(db_url)
             print(f'A problem occured while connecting to the {db_url.DB_URL}')
 
 
